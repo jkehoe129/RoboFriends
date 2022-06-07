141dcdac0d8a6abdbfc20b1863209af02751b3a4
jkehoe129 <84603216+jkehoe129@users.noreply.github.com>
2022-06-07 13:32:38 -0400

    commit

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -5,8 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./containers/App";
 import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
